b3a033c115073a826602fdc2c88388c3d5e1ba09
drruruu ☔ <hello@drruruu.dev>
2021-04-27 12:47:31 -0500

    Update ssh-config.bash

--- Bash/ssh-config.bash
+++ Bash/ssh-config.bash
@@ -1,17 +1,36 @@
 #!/bin/bash
 
-# generates and installs an ssh identity file for GitHub
+#!/usr/bin/env bash
+# sssh-config generates and installs an ssh identity file for a git repository.
 
-email="$(git config --global user.email)"
-if [[ -z "$email" ]]
-then
-  read -p email: email
-fi
+# CAVEAT: Assumes scp-style URLs i.e. relative path w/ no port <dru 2020-04-27>
+declare -A repo
+repo[url]="$1"
+repo[name]="${repo[url]##*/}"
+repo[auth]="${repo[url]%%:*}"
+repo[user]="${repo[auth]%%@*}"
+repo[host]="${repo[auth]##*@}"
 
-# https://git-scm.com/book/en/v2/Git-on-the-Server-Generating-Your-SSH-Public-Key
-ssh-keygen -t rsa -b 4096 -C "$email" -f ~/.ssh/github_rsa
+# CAVEAT: Use HISTCONTROL when invoking <dru 2020-04-27>
+pass="$2"
 
-cat <<EOF >>~/.ssh/config
-host github.com
- IdentityFile ~/.ssh/github_rsa
+# TODO: add ssh-add functionality <dru 2020-04-27>
+# NOBUG: ed25519 not supported by Azure Devops <dru 2020-04-27>
+ssh-keygen -t rsa -b 4096 -C "$(whoami)@$(hostname)" -N "$pass" -f ~/".ssh/${repo[host]}_rsa"
+
+cat <<EOF >~/.ssh/config
+Host ${repo[host]}
+IdentityFile ~/.ssh/%h_rsa
 EOF
+
+# IDEA: Publish public key with API? <dru 2020-04-27>
+#
+# cat ~/.ssh/ssh.dev.azure.com_rsa.pub|clip.exe
+#
+# git init "${repo[name]}"
+# cd "$_"
+#
+# git remote add origin "${repo[url]}"
+# git fetch
+#
+# git checkout -t origin/main
